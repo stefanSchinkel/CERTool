fa57a5fd9604429f33ef078b130f83f81154c124
Stefan Schinkel <schinkel@schinkel>
2013-09-10 11:25:21 +0200

    batchConvert.R: 	script to convert data provided by Andrea (on O:\) 	in large set and store it in ./converted/

---
+++ batchConvert.R
@@ -0,0 +1,48 @@
+# total runtime
+tStart = Sys.time();
+
+cwd = getwd()
+# first read the necessary files
+source('convert.R')
+
+sourceDirectory <- '~/net/O/stefanSchinkel/___AU_Net/RawData/'
+targetDirectory <- '~/work/daten2/11.faces/converted'
+setwd(sourceDirectory)
+
+srcFiles <- list.files(pattern="*210*.")
+for (s in srcFiles){ 
+
+  if (grepl(pattern = '21030',s)) {
+      cat('SKIPPING 22030')  
+      next;
+    }
+    nChars  <- nchar(s)
+    end     <- nChars - 4; # extension and dot
+    start   <- end-4      # should be 5 but not for R so ?!?
+    
+    
+    targetFile <- sprintf('%s/male%s.dat',targetDirectory,substr(s,start,end))
+    convertCERT(s,targetFile)
+
+}
+# be nice and say goodbye
+cat( sprintf('All males took %.1f secs.\n',Sys.time()-tStart) )
+
+srcFiles <- list.files(pattern="*220*.")
+for (s in srcFiles){ 
+  
+  if (grepl(pattern = '22014',s)) {
+    cat('SKIPPING 22030')  
+    next;
+  }
+  nChars  <- nchar(s)
+  end     <- nChars - 4; # extension and dot
+  start   <- end-4      # should be 5 but not for R so ?!?
+  
+  
+  targetFile <- sprintf('%s/female%s.dat',targetDirectory,substr(s,start,end))
+  convertCERT(s,targetFile)
+}
+# be nice and say goodbye
+cat( sprintf('All females took %.1f secs.Goodbye.\n',Sys.time()-tStart) )
+
