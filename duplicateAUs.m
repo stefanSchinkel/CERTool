9ef6472cd536a6e4b28b00da61cfa8703c108c76
Stefan Schinkel <schinkel@schinkel>
2013-09-11 11:21:50 +0200

    splitAUs: 	renamed to duplicateAUs as this is what it does duplicateAUs: 	new name for above, now w/ comments

---
+++ duplicateAUs.m
@@ -0,0 +1,23 @@
+function data = splitAUs(data)
+
+% SPLITAUS - clone CERT channels
+%
+% function data = splitAUs(data)
+%
+% This function duplicates the data for those AUs
+% that are present twice, but for which only one 
+% values is recorded. The input data is taken from
+% importData().
+%
+% Input:
+%	data - 21xN array with activations
+% Output:
+%	data - 25xN array with activations
+%
+% See also: importData.m
+%
+%
+
+idx = [ reshape(repmat(1:7,2,1),1,14) 8:13 14 14 15 16 16 17:21];
+data = data(idx,:);
+
--- splitAUs.m
+++
@@ -1,7 +0,0 @@
-function data = splitAUs(data)
-
-	% 1:7, twice
-	idx = [ reshape(repmat(1:7,2,1),1,14) 8:13 14 14 15 16 16 17:21];
-			 
-	data = data(idx,:);
-	
