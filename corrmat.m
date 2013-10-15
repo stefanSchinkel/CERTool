0060414062f8e3e7604dc55c84048bafcdc00a15
Stefan Schinkel <schinkel@schinkel>
2013-10-15 11:41:12 +0200

    corrmat.m 	- updated help

--- corrmat.m
+++ corrmat.m
@@ -1,7 +1,21 @@
 function r = corrmat(AU)
 
-%CORRMAT (weighted) correlation matrix of AU data
-
+%CORRMAT correlation matrix of AU data
+%
+% r = corrmat(AU)
+%
+% Compute the all to all correlation matrix of an  
+% MxN array of a AU activations where M is the index 
+% of the AU and N is sampling time. 
+%
+% Input:
+%	AU -  AU data (MxN Marix)
+%
+% Output:
+%	r - all to all correlation matrix
+%
+% See also: CERTool
+%
 r = corrcoef(AU');
 
 
